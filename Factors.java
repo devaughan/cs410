8a428c02a67a829b4e8d672cfc28112bf363da18
devaughan <75347649+devaughan@users.noreply.github.com>
2021-03-18 01:51:56 -0400

    Add files via upload

--- Factors.java
+++ Factors.java
@@ -6,7 +6,11 @@ public class Factors {
 		
 		Scanner input = new Scanner(System.in);
 	
-		int upperLimit = 0;
+		int upperLimit = 0; // stores the user inputted max
+		int product; // stores the product
+		int left; // first multiple
+		int right; // second mutliple
+		//int[][] answer = {};
 		
 		
 		while (upperLimit < 4) {
@@ -14,15 +18,17 @@ public class Factors {
 			upperLimit = input.nextInt();
 		}
 		
-		
-		
-		for (int product = 0; product <= upperLimit; product++) {
-			for (int c = 2; c <= upperLimit / 2; c++) {
-				for (int x = 2; x <= upperLimit / 2; x++) {
-					if (c * x == product)
-						System.out.println(product + " = " + c + " x " + x);
+		for (product = 4; product <= upperLimit; product++) {
+			
+			for (left = 2; left < upperLimit / 2; left++) {
+				for (right = 2; right <= Math.sqrt(product); right++) {
+					if (left * right == product) {
+						//answer.add({left, right});
+						System.out.println(product + " = " + left + " x " + right);
+					}
 				}
-			}				
+			}
 		}
+		
 	}
 }
